d9ff03a412da67fef0fd4e3ffb0604514f2f7895
Alexander Stefanov <alexander.stefanov@lulin.bg>
2015-03-16 19:44:54 +0200

    Changed DEFAULT_FROM_EMAIL to admin@veritza.org

--- veritza/settings/prod.py
+++ veritza/settings/prod.py
@@ -35,7 +35,7 @@ EMAIL_USE_TLS = True
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = EMAIL_HOST_USER
 
-DEFAULT_FROM_EMAIL='alexander.stefanov@lulin.bg'
+DEFAULT_FROM_EMAIL='admin@veritza.org'
 ########## END EMAIL CONFIGURATION
 
 
