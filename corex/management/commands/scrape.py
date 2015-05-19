1a81826dcf4d4879d72db35ddf6ed80963b994b1
Matt Gathu <mattgathu@gmail.com>
2015-05-19 15:40:55 +0300

    moved management commands to core app

---
+++ corex/management/commands/scrape.py
@@ -0,0 +1,55 @@
+"""
+Scraping commands
+
+"""
+from collections import deque
+
+from django.core.management.base import BaseCommand
+from serbia.scrapers.companies import SerbiaCompanyScraper as Scc
+from serbia.scrapers.officials import SerbiaOfficialsScraper as Sos
+from usa.procurement import FedContractsScraper as Fcs
+
+
+class ScraperScheduler:
+    """Schedule scraping tasks
+
+    """
+    def __init__(self):
+        """Initiliaze task queue"""
+        self._task_queue = deque()
+
+    def scraping_task(self, task):
+        """Admit a newly started task to the scheduler
+
+        """
+        self._task_queue.append(task)
+
+    def run(self):
+        """Execute unit there are no more tasks
+
+        """
+        while self._task_queue:
+            task = self._task_queue.popleft()
+            try:
+                next(task)
+                self._task_queue.append(task)
+            except StopIteration:
+                pass
+
+
+class Command(BaseCommand):
+    """Scraping Command"""
+    help = 'start scrapers'
+
+    def handle(self, *args, **options):
+        """run command"""
+        # initialize scheduler
+        sched = ScraperScheduler()
+
+        # add scraping tasks
+        sched.scraping_task(Fcs().run())
+        sched.scraping_task(Sos().run())
+        sched.scraping_task(Scc().run())
+
+        # run scheduler
+        sched.run()
