31a1a28c31a91fe4b8c425551c00d387c6e0d753
Matt Gathu <mattgathu@gmail.com>
2015-06-04 16:44:16 +0300

    enhancement: added auto_retry decorator
    This decorator allows for a routine to retry after a specified time
    delay, usually when a certain exception has occurred.

--- corex/utils.py
+++ corex/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Helper Module
 
@@ -5,9 +6,30 @@ __author__ = Matt Gathu <mattgathu@gmail.com>
 __date__  = April 2015
 
 """
+import time
+from functools import wraps
+
 
 def flatten_list(lst):
     """flatten a list of lists"""
     lst = [item for sublist in lst for item in sublist]
 
     return lst
+
+
+def auto_retry(tries=3, exc=Exception, delay=5):
+    """retry decorator factory"""
+    def decorator(func):
+        """function decorator"""
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """func wrapper"""
+            for _ in range(tries):
+                try:
+                    return func(*args, **kwargs)
+                except exc:
+                    time.sleep(delay)
+                    continue
+            raise exc
+        return wrapper
+    return decorator
