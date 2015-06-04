183209d375ced75df8aafa5400ad04272f5d6dec
Matt Gathu <mattgathu@gmail.com>
2015-06-04 16:29:06 +0300

    enhacement: added FedSecFiling model
    This database model represents data of publicly traded companies
    filings from the Securities and Exchange Commission.

--- usa/models.py
+++ usa/models.py
@@ -41,6 +41,41 @@ class FedFinancialDisclosure(cmodels.BaseModel):
     pdf = models.CharField(max_length=255)
     year = models.CharField(max_length=255)
 
+
+class FedSecFiling(cmodels.BaseModel):
+    """Federal Securities and Exchange Commission filing model"""
+    accession_number = models.CharField(max_length=255, null=True)
+    business_address = models.CharField(max_length=255, null=True)
+    business_phone = models.CharField(max_length=255, null=True)
+    central_index_key = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    company_conformed_name = models.CharField(max_length=255, null=True)
+    company_data = models.CharField(max_length=255, null=True)
+    conformed_submission_type = models.CharField(max_length=255, null=True)
+    date_as_of_change = models.CharField(max_length=255, null=True)
+    date_of_name_change = models.CharField(max_length=255, null=True)
+    filed_as_of_date = models.CharField(max_length=255, null=True)
+    filer = models.CharField(max_length=255, null=True)
+    filing_values = models.CharField(max_length=255, null=True)
+    film_number = models.CharField(max_length=255, null=True)
+    fiscal_year_end = models.CharField(max_length=255, null=True)
+    form_type = models.CharField(max_length=255, null=True)
+    former_company = models.CharField(max_length=255, null=True)
+    former_conformed_name = models.CharField(max_length=255, null=True)
+    irs_number = models.CharField(max_length=255, null=True)
+    mail_address = models.CharField(max_length=255, null=True)
+    public_document_count = models.CharField(max_length=255, null=True)
+    sec_act = models.CharField(max_length=255, null=True)
+    sec_file_number = models.CharField(max_length=255, null=True)
+    standard_industrial_classification = models.CharField(max_length=255, null=True)
+    state = models.CharField(max_length=255, null=True)
+    state_of_incorporation = models.CharField(max_length=255, null=True)
+    street_1 = models.CharField(max_length=255, null=True)
+    street_2 = models.CharField(max_length=255, null=True)
+    url = models.CharField(max_length=255, null=True)
+    zip = models.CharField(max_length=255, null=True)
+
+
 # ============================================================================
 # EOF
 # ============================================================================
