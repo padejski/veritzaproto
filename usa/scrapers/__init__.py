c2cd52a02491ce341f7f0b7a3e4ec0e4e7b15a8f
Matt Gathu <mattgathu@gmail.com>
2015-06-08 12:02:18 +0300

    added elections scrapers to scrapers list

--- usa/scrapers/__init__.py
+++ usa/scrapers/__init__.py
@@ -2,8 +2,22 @@
 US Federal Level Scrapers
 
 """
+# ============================================================================
+# import scrapers
+# ============================================================================
 from sec_edgar import SecEdgarScraper
 from procurement import FedContractsScraper
 from financial_disclosure import FinDisclosuresScraper
+from elections import CommitteeContributionsScraper, ElectionCandsScraper
+from elections import IndividualContributionsScraper
 
-usa_scrapers = [FedContractsScraper, FinDisclosuresScraper, SecEdgarScraper]
+# ============================================================================
+# scrapers list
+# ============================================================================
+usa_scrapers = [FedContractsScraper, FinDisclosuresScraper, SecEdgarScraper,
+                CommitteeContributionsScraper, ElectionCandsScraper,
+                IndividualContributionsScraper]
+
+# ============================================================================
+# EOF
+# ============================================================================
