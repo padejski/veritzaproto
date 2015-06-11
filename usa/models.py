b44a9422e578765604ad649fb25d18b8054b18b9
Matt Gathu <mattgathu@gmail.com>
2015-06-11 09:14:44 +0300

    enhancement: FedToxicsFacility db model
    Database model for the Toxics Inventory Scraper

--- usa/models.py
+++ usa/models.py
@@ -156,6 +156,49 @@ class FedSecFiling(cmodels.BaseModel):
     zip = models.CharField(max_length=255, null=True)
 
 
+class FedToxicsFacility(cmodels.BaseModel):
+    """Federal Toxics Facility Model"""
+    accuracy_value = models.CharField(max_length=255, null=True)
+    census_block_code = models.CharField(max_length=255, null=True)
+    city_name = models.CharField(max_length=255, null=True, verbose_name='City')
+    collect_desc = models.CharField(max_length=255, null=True)
+    congressional_dist_num = models.CharField(max_length=255, null=True, verbose_name='')
+    conveyor = models.CharField(max_length=255, null=True)
+    country_name = models.CharField(max_length=255, null=True, verbose_name='Country')
+    county_name = models.CharField(max_length=255, null=True, verbose_name='County')
+    create_date = models.CharField(max_length=255, null=True, verbose_name='Creation Date')
+    epa_region_code = models.CharField(max_length=255, null=True, verbose_name='EPA Region Code')
+    federal_agency_name = models.CharField(max_length=255, null=True, verbose_name='Agency')
+    federal_facility_code = models.CharField(max_length=255, null=True, verbose_name='Facility Code')
+    fips_code = models.CharField(max_length=255, null=True, verbose_name='FIPS Code')
+    frs_facility_detail_report_url = models.CharField(max_length=255, null=True, verbose_name='Report URL')
+    hdatum_desc = models.CharField(max_length=255, null=True)
+    huc_code = models.CharField(max_length=255, null=True, verbose_name='HUC Code')
+    interest_types = models.CharField(max_length=255, null=True)
+    latitude83 = models.CharField(max_length=255, null=True)
+    location_address = models.CharField(max_length=255, null=True, verbose_name='Location')
+    location_description = models.CharField(max_length=255, null=True)
+    longitude83 = models.CharField(max_length=255, null=True)
+    naics_code_descriptions = models.CharField(max_length=255, null=True)
+    naics_codes = models.CharField(max_length=255, null=True)
+    pgm_sys_acrnms = models.CharField(max_length=255, null=True)
+    postal_code = models.CharField(max_length=255, null=True)
+    primary_name = models.CharField(max_length=255, null=True, verbose_name='Primary Name')
+    ref_point_desc = models.CharField(max_length=255, null=True)
+    registry_id = models.CharField(max_length=255, null=True, verbose_name='Registry ID')
+    sic_code_descriptions = models.CharField(max_length=255, null=True)
+    sic_codes = models.CharField(max_length=255, null=True)
+    site_type_name = models.CharField(max_length=255, null=True)
+    source_desc = models.CharField(max_length=255, null=True)
+    state_code = models.CharField(max_length=255, null=True)
+    state_name = models.CharField(max_length=255, null=True, verbose_name='State')
+    supplemental_location = models.CharField(max_length=255, null=True)
+    tribal_land_code = models.CharField(max_length=255, null=True)
+    tribal_land_name = models.CharField(max_length=255, null=True, verbose_name='Tribal Land Name')
+    update_date = models.CharField(max_length=255, null=True, verbose_name='Update Date')
+    us_mexico_border_ind = models.CharField(max_length=255, null=True)
+
+
 # ============================================================================
 # EOF
 # ============================================================================
