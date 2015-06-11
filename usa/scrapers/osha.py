bcc0d950196211dd8484a1f381d8126cacbad1aa
Matt Gathu <mattgathu@gmail.com>
2015-06-11 11:45:55 +0300

    enhancement: Osha Inpsection Scraper
    Scraper for OSHA Inspection data

--- usa/scrapers/osha.py
+++ usa/scrapers/osha.py
@@ -12,7 +12,7 @@ __date__ = June 2015
 # necessary imports
 # ============================================================================
 from corex.basescraper import BaseScraper
-from ..models import FedOshaEbsa
+from ..models import FedOshaEbsa, FedOshaInspection
 
 # ============================================================================
 # useful constants
@@ -21,7 +21,7 @@ EBSA_URL = 'http://prd-enforce-xfr-02.dol.gov/data_catalog/EBSA/ebsa_ocats_20150
 EBSA_HEADERS = ['case_type', 'ein', 'pn', 'plan_year', 'plan_name', 'plan_admin',
                 'plan_admin_state', 'plan_admin_zip_code', 'final_close_reason',
                 'final_close_date', 'penalty_amount']
-
+INSP_URL = 'http://prd-enforce-xfr-02.dol.gov/data_catalog/OSHA/osha_inspection_20150610.csv.zip'
 
 
 # ============================================================================
@@ -71,6 +71,58 @@ class OshaEbsaScraper(BaseScraper):
         for data in self.fetch_data():
             data = FedOshaEbsa(**data)
             self.save_model(data)
+
+            yield
+
+
+class OshaInspectionScraper(BaseScraper):
+    """OSHA Inspection data scraper"""
+
+    def __init__(self):
+        """init base class"""
+        BaseScraper.__init__(self)
+
+    def download_file(self):
+        """download and deflate zip data file"""
+        zfile = self.download_http(INSP_URL, zfile=True)
+        fname = zfile.namelist()[0]
+
+        return zfile.open(fname)
+
+    def fetch_data(self):
+        """fetch data from OSHA site"""
+        for data in self.gen_data_dicts(self.download_file()):
+            yield data
+
+    def gen_data_dicts(self, dfile):
+        """generate data dictionaries
+
+        args:
+            dfile: data csv file
+
+        yields:
+            data (dict): data dictionary
+
+        """
+        lines = (line.split(',') for line in dfile)
+
+        # get csv headers
+        headers = next(lines)
+
+        for line in lines:
+            data = dict(zip(headers, line))
+            data['hash'] = self.get_hash(data)
+
+            yield data
+
+    def run(self):
+        """run scraper"""
+        for data in self.fetch_data():
+            data = FedOshaInspection(**data)
+            self.save_model(data)
+
+            yield
+
 
 # ============================================================================
 # EOF
