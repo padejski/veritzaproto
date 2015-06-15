cbef200f0b1633a8486bfbddc703d68d4ac816d1
Matt Gathu <mattgathu@gmail.com>
2015-06-15 23:52:14 +0300

    enhancment: new views for new datasets

--- usa/tables.py
+++ usa/tables.py
@@ -9,6 +9,16 @@ import django_tables2 as tables
 from . import models
 
 
+class CandidateTable(tables.Table):
+    """election candidate table"""
+    class Meta:
+        models = models.FedCandidate
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'cand_id', 'cand_st2',
+                   'cand_office_st', 'cand_ici', 'cand_pcc', 'cand_st1',
+                   'cand_zip', 'url', 'basemodel_ptr')
+
+
 class CompanyTable(tables.Table):
     """companies data table"""
     class Meta:
@@ -17,6 +27,47 @@ class CompanyTable(tables.Table):
         exclude = ('id', 'created', 'updated', 'hash', 'name', 'directors',
                    'address', 'duns_num',  'status', 'url', 'basemodel_ptr',
                    'companybasemodel_ptr', 'other', 'alt_address', 'industry')
+
+
+class CommitteeContributionTable(tables.Table):
+    """committee contributions  data table"""
+    class Meta:
+        model = models.FedCommitteeContribution
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'amndt_ind', 'memo_cd',
+                   'sub_id', 'tran_id', 'transaction_pgi', 'zip_code',
+                   'memo_text', 'other_id',  'status', 'url', 'basemodel_ptr',
+                   'image_num', 'other', 'occupation', 'industry', 'employer',
+                   'candidate', 'entity_tp', 'transaction_tp')
+
+
+class FinancialDisclosureTable(tables.Table):
+    """Financial disclosures  data table"""
+    class Meta:
+        model = models.FedFinancialDisclosure
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr')
+
+
+class IndividualContributionTable(tables.Table):
+    """Individual contributions  data table"""
+    class Meta:
+        model = models.FedIndividualContribution
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'amndt_ind', 'memo_cd',
+                   'sub_id', 'tran_id', 'transaction_pgi', 'zip_code',
+                   'memo_text', 'other_id',  'status', 'url', 'basemodel_ptr',
+                   'image_num', 'other', 'occupation', 'employer', 'cmte_id',
+                   'rpt_tp', 'entity_tp', 'transaction_tp')
+
+
+class IrsExemptOrgTable(tables.Table):
+    """IRS Exempt Organizations  data table"""
+    class Meta:
+        model = models.FedIrsExemptOrg
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr')
+
 
 
 class OfficialTable(tables.Table):
@@ -35,8 +86,81 @@ class ProcurementTable(tables.Table):
         model = models.FedProcurement
         attrs = {"class": "paleblue table table-striped table-bordered"}
         exclude = ('id', 'created', 'updated', 'hash', 'transaction_id',
-                   'contact_person', 'basemodel_ptr', 'url'
+                   'contact_person', 'basemodel_ptr', 'url',
                    'procurementbasemodel_ptr', 'other')
+
+
+class SecFilingTable(tables.Table):
+    """SEC filings  data table"""
+    class Meta:
+        model = models.FedSecFiling
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr',
+                   'accession_number', 'business_phone', 'central_index_key',
+                   'company_data', 'date_as_of_change', 'date_of_name_change',
+                   'former_company', 'former_conformed_name', 'public_document_count',
+                   'sec_act', 'standard_industrial_classification', 'street_2',
+                   'zip', 'business_address', 'filer', 'fiscal_year_end',
+                   'conformed_submission_type', 'mail_address',
+                   'state_of_incorporation', 'street_1', 'filing_values')
+
+
+class ToxicsFacilityTable(tables.Table):
+    """Toxics facilities  data table"""
+    class Meta:
+        model = models.FedToxicsFacility
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr',
+                   'accuracy_value', 'census_block_code', 'collect_desc',
+                   'congressional_dist_num', 'conveyor', 'epa_region_code',
+                   'federal_facility_code', 'hdatum_desc', 'huc_code',
+                   'interest_types', 'latitude83', 'location_description',
+                   'longitude83', 'naics_code_descriptions', 'pgm_sys_acrnms',
+                   'ref_point_desc', 'sic_code_descriptions', 'source_desc',
+                   'state_code', 'supplemental_location', 'tribal_land_name',
+                   'tribal_land_code', 'tribal_land_name', 'us_mexico_border_ind')
+
+
+class OshaEbsaTable(tables.Table):
+    """OSHA Ebsa Enforcement data table"""
+    class Meta:
+        model = models.FedOshaEbsa
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr')
+
+
+class OshaInspectionTable(tables.Table):
+    """OSHA Inspection  data table"""
+    class Meta:
+        model = models.FedOshaInspection
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr',
+                   'case_mod_date', 'close_case_date', 'close_conf_date',
+                   'health_const', 'health_manuf', 'health_marit',
+                   'host_est_key', 'indstry_dim_id', 'insp_type', 'insp_scope',
+                   'ld_dt', 'mail_city', 'mail_state', 'mail_street', 'mail_zip',
+                   'migrant', 'nr_in_estab', 'owner_type', 'safety_hlth',
+                   'safety_const', 'safety_manuf', 'safety_marit', 'site_address',
+                   'site_zip', 'state_flag', 'union_status', 'zip_dim_id')
+
+
+class CpscRecallTable(tables.Table):
+    """Consumer Product Safety Commission Recall data table"""
+    class Meta:
+        model = models.FedCpscRecall
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr',
+                   'consumer_contact', 'hazards', 'images', 'in_conj', 'injuries',
+                   'mfcs_countries', 'product_upcs', 'remedies', 'desc')
+
+
+class CpscRecallViolationTable(tables.Table):
+    """Consumer Product Safety Commission Recall Violation data table"""
+    class Meta:
+        model = models.FedCpscRecallViolation
+        attrs = {"class": "paleblue table table-striped table-bordered"}
+        exclude = ('id', 'created', 'updated', 'hash', 'url', 'basemodel_ptr',
+                   'address_2')
 
 
 # ============================================================================
--- usa/views.py
+++ usa/views.py
@@ -8,11 +8,26 @@ from django_tables2 import SingleTableView
 from . import models, tables
 
 
+class CandidatesView(SingleTableView):
+    """list of candidates view"""
+    model = models.FedCandidate
+    table_class = tables.CandidateTable
+    template_name = 'usa_list.html'
+
+
 class CompaniesView(SingleTableView):
     """list of companies view"""
     model = models.FedCompany
     table_class = tables.CompanyTable
-    template_name = 'usa_companies.html'
+    template_name = 'usa_list.html'
+
+class CommitteeContributionsView(SingleTableView):
+    """list of committee contributions"""
+    model = models.FedCommitteeContribution
+    table_class = tables.CommitteeContributionTable
+    template_name = 'usa_list.html'
+
+
 
 
 class DatasetsView(TemplateView):
@@ -22,21 +37,94 @@ class DatasetsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DatasetsView, self).get_context_data(**kwargs)
         context['companies_no'] = models.FedCompany.objects.count()
+        context['disclosures_no'] = models.FedFinancialDisclosure.objects.count()
+        context['procurements_no'] = models.FedProcurement.objects.count()
+        context['irsexempt_no'] = models.FedIrsExemptOrg.objects.count()
+        context['secfilings_no'] = models.FedSecFiling.objects.count()
+        context['toxics_no'] = models.FedToxicsFacility.objects.count()
+        context['oshaebsa_no'] = models.FedOshaEbsa.objects.count()
+        context['oshainsp_no'] = models.FedOshaInspection.objects.count()
+        context['cpscrecalls_no'] = models.FedCpscRecall.objects.count()
+        context['cpscviol_no'] = models.FedCpscRecallViolation.objects.count()
+        context['candidates_no'] = models.FedCandidate.objects.count()
+        context['commcontribs_no'] = models.FedCommitteeContribution.objects.count()
+        context['indvcontribs_no'] = models.FedIndividualContribution.objects.count()
         context['officials_no'] = 0
 
         return context
 
+class FinancialDisclosuresView(SingleTableView):
+    """list of Financial disclosures"""
+    model = models.FedFinancialDisclosure
+    table_class = tables.FinancialDisclosureTable
+    template_name = 'usa_list.html'
 
 class IndexView(TemplateView):
     """Index view """
     template_name = 'usa_home.html'
 
 
+class IndividualContributionsView(SingleTableView):
+    """list of Individual contributions"""
+    model = models.FedIndividualContribution
+    table_class = tables.IndividualContributionTable
+    template_name = 'usa_list.html'
+
+
+class IrsExemptOrgsView(SingleTableView):
+    """list of IRS Exempt Organizations"""
+    model = models.FedIrsExemptOrg
+    table_class = tables.IrsExemptOrgTable
+    template_name = 'usa_list.html'
+
+
 class ProcurementsView(SingleTableView):
     """List of procurements """
     model = models.FedProcurement
     table_class = tables.ProcurementTable
-    template_name = 'usa_procurements.html'
+    template_name = 'usa_list.html'
+
+
+class SecFilingsView(SingleTableView):
+    """list of SEC filings"""
+    model = models.FedSecFiling
+    table_class = tables.SecFilingTable
+    template_name = 'usa_list.html'
+
+
+class ToxicsFacilitiesView(SingleTableView):
+    """list of Toxics facilities"""
+    model = models.FedToxicsFacility
+    table_class = tables.ToxicsFacilityTable
+    template_name = 'usa_list.html'
+
+
+class OshaEbsasView(SingleTableView):
+    """list of OSHA EBSA"""
+    model = models.FedOshaEbsa
+    table_class = tables.OshaEbsaTable
+    template_name = 'usa_list.html'
+
+
+class OshaInspectionsView(SingleTableView):
+    """list of OSHA inspections"""
+    model = models.FedOshaInspection
+    table_class = tables.OshaInspectionTable
+    template_name = 'usa_list.html'
+
+
+class CpscRecallsView(SingleTableView):
+    """list of CPSC Recalls"""
+    model = models.FedCpscRecall
+    table_class = tables.CpscRecallTable
+    template_name = 'usa_list.html'
+
+
+class CpscRecallViolationsView(SingleTableView):
+    """list of CPSC Recall Violations"""
+    model = models.FedCpscRecallViolation
+    table_class = tables.CpscRecallViolationTable
+    template_name = 'usa_list.html'
 # ============================================================================
 # EOF
 # ============================================================================
