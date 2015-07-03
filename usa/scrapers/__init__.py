2177e7339fa8cd323a53d449a9b26fb962bbe74a
Matt Gathu <mattgathu@gmail.com>
2015-07-03 22:33:03 +0300

    updated scrapers list

--- usa/scrapers/__init__.py
+++ usa/scrapers/__init__.py
@@ -10,13 +10,21 @@ from procurement import FedContractsScraper
 from financial_disclosure import FinDisclosuresScraper
 from elections import CommitteeContributionsScraper, ElectionCandsScraper
 from elections import IndividualContributionsScraper
+from cpsc import CpscRecallsScraper, CpscRecallsViolationsScraper
+from irs_exempt import IrsExemptScraper
+from osha  import OshaEbsaScraper, OshaInspectionScraper
+from sec_edgar import SecEdgarScraper
+from toxics_inventory import ToxicsInventoryScraper
 
 # ============================================================================
 # scrapers list
 # ============================================================================
 usa_scrapers = [FedContractsScraper, FinDisclosuresScraper, SecEdgarScraper,
                 CommitteeContributionsScraper, ElectionCandsScraper,
-                IndividualContributionsScraper]
+                IndividualContributionsScraper, CpscRecallsScraper,
+                CpscRecallsViolationsScraper, IrsExemptScraper,
+                OshaEbsaScraper, OshaInspectionScraper, SecEdgarScraper,
+                ToxicsInventoryScraper]
 
 # ============================================================================
 # EOF
