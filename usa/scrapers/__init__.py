8a69bfe38982fbb394b60cbc8435b7fc6439bb9c
Matt Gathu <mattgathu@gmail.com>
2015-07-16 15:28:24 +0300

    debug: returned all scrapers to scraper list
    Resolved scraper bug issues and restored the full scrapers
    list.

--- usa/scrapers/__init__.py
+++ usa/scrapers/__init__.py
@@ -6,20 +6,19 @@ US Federal Level Scrapers
 # import scrapers
 # ============================================================================
 from sec_edgar import SecEdgarScraper
-# from procurement import FedContractsScraper
+from procurement import FedContractsScraper
 from financial_disclosure import FinDisclosuresScraper
 from elections import CommitteeContributionsScraper, ElectionCandsScraper
 from elections import IndividualContributionsScraper
 from cpsc import CpscRecallsScraper, CpscRecallsViolationsScraper
 from irs_exempt import IrsExemptScraper
 from osha  import OshaEbsaScraper, OshaInspectionScraper
-from sec_edgar import SecEdgarScraper
 from toxics_inventory import ToxicsInventoryScraper
 
 # ============================================================================
 # scrapers list
 # ============================================================================
-usa_scrapers = [SecEdgarScraper,
+usa_scrapers = [SecEdgarScraper, FedContractsScraper,
                 CommitteeContributionsScraper, ElectionCandsScraper,
                 IndividualContributionsScraper, CpscRecallsScraper,
                 CpscRecallsViolationsScraper, IrsExemptScraper,
