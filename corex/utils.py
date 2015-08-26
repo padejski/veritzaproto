65a344016899f5d8a68f5edfa9281cb623ae0c74
Matt Gathu <mattgathu@gmail.com>
2015-08-26 19:06:17 +0300

    enhancement: added utility functions
    added two utility functions
    
    are_similar: string difference check with 0.9 similarity index
    get_hash: generate md5 digest of passed object

--- corex/utils.py
+++ corex/utils.py
@@ -6,10 +6,18 @@ __author__ = Matt Gathu <mattgathu@gmail.com>
 __date__  = April 2015
 
 """
+# ============================================================================
+# imports
+# ============================================================================
 import time
 from functools import wraps
+from hashlib import md5
+from difflib import SequenceMatcher
 
 
+# ============================================================================
+# utility functions
+# ============================================================================
 def flatten_list(lst):
     """flatten a list of lists"""
     lst = [item for sublist in lst for item in sublist]
@@ -33,3 +41,23 @@ def auto_retry(tries=3, exc=Exception, delay=5):
             raise exc
         return wrapper
     return decorator
+
+
+def are_similar(str1, str2, index=0.9):
+    """Check if two strings are similar using a similarity index/ratio
+
+    are_similar(str1, str2, index=0.9) -> True/False
+
+    """
+    if SequenceMatcher(None, str1, str2).ratio() >= index:
+        return True
+
+    return False
+
+
+def get_hash(obj):
+    """get md5 digest of obj"""
+    return md5(str(obj)).digest().decode("iso-8859-1")
+# ============================================================================
+# EOF
+# ============================================================================
