6bd9decd2c35ee7f86f521681e7826a1984f0c26
Matt Gathu <mattgathu@gmail.com>
2015-09-08 18:17:20 +0300

    refactor: simplied integration code
    moved out individual integrations into functions and also moved
    database interaction code to a single function to reduce code
    duplication.

--- corex/management/commands/integratedata.py
+++ corex/management/commands/integratedata.py
@@ -12,6 +12,82 @@ import serbia.models
 from corex.utils import are_similar, get_hash
 
 
+# ============================================================================
+# helpers
+# ============================================================================
+def integrate_officials_companies():
+    """integrate public officials and companies
+
+    """
+    officials = serbia.models.Official.objects.all()
+    companies = serbia.models.Company.objects.all()
+
+    for off in officials:
+        for cmpy in companies:
+            for individual in cmpy.individuals:
+                if are_similar(off.name, individual):
+                    dat = {'official': off, 'company': cmpy}
+                    make_save_model(serbia.models.OfficialCompany, dat)
+
+                    break
+
+
+def integrate_officialscompanies_procurement():
+    """integrate public officials companies in procurement
+
+    """
+    officialcompanies = serbia.models.OfficialCompany.objects.all()
+    procurements = serbia.models.Procurement.objects.all()
+
+    for offcmp in officialcompanies:
+        for proc in procurements:
+            if are_similar(offcmp.company.company_id, proc.supplier_id.strip()):
+                dat = {'official': offcmp.official,
+                       'company': offcmp.company,
+                       'procurement': proc}
+                make_save_model(serbia.models.OfficialCompanyProcurement, dat)
+
+
+def integrate_politicalfunders_companies():
+    """integrate political funders with companies
+
+    """
+    companies = serbia.models.Company.objects.all()
+    donations = serbia.models.ElectionDonation.objects.all()
+
+    for cmpy in companies:
+        for donation in donations:
+            for individual in cmpy.individuals:
+                if are_similar(individual, donation.donor_name):
+                    dat = {'company': cmpy, 'donation': donation}
+                    make_save_model(serbia.models.PoliticalFunderCompany, dat)
+
+                    break
+
+
+def make_save_model(model, attr_dict):
+    """make database model instance using attributes dictionary and save it to
+    the database.
+
+    make_save_model(model_class, atrributes_dict) -> True / False
+
+    returns:
+        True / False (boolean): value depends on if database transaction is
+                                successful.
+    """
+    attr_dict['hash'] = get_hash(attr_dict)
+    model = model(**attr_dict)
+
+    try:
+        model.save()
+        return True
+    except IntegrityError:
+        return False
+
+
+# ============================================================================
+# data integration: django command
+# ============================================================================
 class Command(BaseCommand):
     """integration command
 
@@ -24,66 +100,17 @@ class Command(BaseCommand):
         # ====================================================================
         # public officials and companies integrations
         # ====================================================================
-        officials = serbia.models.Official.objects.all()
-        companies = serbia.models.Company.objects.all()
+        integrate_officials_companies()
 
-        for off in officials:
-            for cmpy in companies:
-                for individual in cmpy.individuals:
-                    if are_similar(off.name, individual):
-                        dat = {'official': off, 'company': cmpy}
-                        dat['hash'] = get_hash(dat)
-                        model = serbia.models.OfficialCompany(**dat)
+        # ====================================================================
+        # public officials companies in procurement integrations
+        # ====================================================================
+        integrate_officialscompanies_procurement()
 
-                        try:
-                            model.save()
-                        except IntegrityError:
-                            pass
-
-                        break
-
-    # ========================================================================
-    # public officials companies in procurement integrations
-    # ========================================================================
-    officialcompanies = serbia.models.OfficialCompany.objects.all()
-    procurements = serbia.models.Procurement.objects.all()
-
-    for offcmp in officialcompanies:
-        for proc in procurements:
-            if are_similar(offcmp.company.company_id, proc.supplier_id.strip()):
-                dat = {'official': offcmp.official,
-                       'company': offcmp.company,
-                       'procurement': proc}
-                dat['hash'] = get_hash(dat)
-                model = serbia.models.OfficialCompanyProcurement(**dat)
-
-                try:
-                    model.save()
-                except IntegrityError:
-                    pass
-
-    # ========================================================================
-    # political funders companies
-    # ========================================================================
-    companies = serbia.models.Company.objects.all()
-    donations = serbia.models.ElectionDonation.objects.all()
-
-    for cmpy in companies:
-        for donation in donations:
-            for individual in cmpy.individuals:
-                if are_similar(individual, donation.donor_name):
-                    dat = {'company': cmpy, 'donation': donation}
-                    dat['hash'] = get_hash(dat)
-                    model = serbia.models.PoliticalFunderCompany(**dat)
-
-                    try:
-                        model.save()
-                    except IntegrityError:
-                        pass
-
-                    break
-
-
+        # ====================================================================
+        # political funders companies
+        # ====================================================================
+        integrate_politicalfunders_companies()
 # ============================================================================
 # EOF
 # ============================================================================
