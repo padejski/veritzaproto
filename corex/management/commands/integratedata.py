4e7281f8964cbef7db275b1f9f6bc666397b3ce9
Matt Gathu <mattgathu@gmail.com>
2015-09-14 10:12:58 +0300

    enhancement: added new integration functions

--- corex/management/commands/integratedata.py
+++ corex/management/commands/integratedata.py
@@ -13,7 +13,7 @@ from corex.utils import are_similar, get_hash
 
 
 # ============================================================================
-# helpers
+# helper functions
 # ============================================================================
 def integrate_officials_companies():
     """integrate public officials and companies
@@ -41,14 +41,14 @@ def integrate_officialscompanies_procurement():
 
     for offcmp in officialcompanies:
         for proc in procurements:
-            if are_similar(offcmp.company.company_id, proc.supplier_id.strip()):
+            if are_similar(offcmp.company.company_id, proc.supplier_id):
                 dat = {'official': offcmp.official,
                        'company': offcmp.company,
                        'procurement': proc}
                 make_save_model(serbia.models.OfficialCompanyProcurement, dat)
 
 
-def integrate_politicalfunders_companies():
+def integrate_funders_companies():
     """integrate political funders with companies
 
     """
@@ -60,9 +60,38 @@ def integrate_politicalfunders_companies():
             for individual in cmpy.individuals:
                 if are_similar(individual, donation.donor_name):
                     dat = {'company': cmpy, 'donation': donation}
-                    make_save_model(serbia.models.PoliticalFunderCompany, dat)
+                    make_save_model(serbia.models.FunderCompany, dat)
 
                     break
+
+
+
+def integrate_funderscompanies_procurement():
+    """ integrate political funders companies with procurement
+
+    """
+    funderscompanies = serbia.models.FunderCompany.objects.all()
+    procurements = serbia.models.Procurement.objects.all()
+
+    for cmpy in funderscompanies:
+        for proc in procurements:
+            if are_similar(cmpy.company.company_id, proc.supplier_id):
+                dat = {'company': cmpy, 'procurement': proc}
+                make_save_model(serbia.models.FunderCompanyProcurement, dat)
+
+
+def integrate_fundingcompanies_procurement():
+    """integrate comapnies that are political funders with procurement
+
+    """
+    companies = serbia.models.FundingCompany.objects.all()
+    procurements = serbia.models.Procurement.objects.all()
+
+    for cmpy in companies:
+        for proc in procurements:
+            if are_similar(cmpy.company.company_id, proc.supplier_id):
+                dat = {'company': cmpy, 'procurement': proc}
+                make_save_model(serbia.models.FundingCompanyProcurement, dat)
 
 
 def make_save_model(model, attr_dict):
@@ -110,7 +139,17 @@ class Command(BaseCommand):
         # ====================================================================
         # political funders companies
         # ====================================================================
-        integrate_politicalfunders_companies()
+        integrate_funders_companies()
+
+        # ====================================================================
+        # political funders companies in procurement
+        # ====================================================================
+        integrate_funderscompanies_procurement()
+
+        # ====================================================================
+        # funding companies in procurement
+        # ====================================================================
+        integrate_fundingcompanies_procurement()
 # ============================================================================
 # EOF
 # ============================================================================
