0a971c471cab7dc37af8234683c0209c08ba56ca
Matt Gathu <mattgathu@gmail.com>
2015-12-22 23:21:30 +0300

    adding scrape trackers to scraping scheduler

--- apps/corex/management/commands/scrape.py
+++ apps/corex/management/commands/scrape.py
@@ -8,11 +8,13 @@ Scraping commands
 import sys
 from itertools import chain
 from collections import deque
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 
 from apps.serbia.scrapers import serbia_scrapers
 from apps.usa.scrapers import usa_scrapers
+from apps.corex.models import ScrapeTracker
 
 # ============================================================================
 # scrapers list
@@ -42,8 +44,8 @@ class ScraperScheduler:
         while self._task_queue:
             task = self._task_queue.popleft()
             try:
-                self._task_queue.append(task)
                 next(task)
+                self._task_queue.append(task)
             except StopIteration:
                 continue
             except Exception as exc:
@@ -55,18 +57,37 @@ class Command(BaseCommand):
     """Scraping Command"""
     help = 'start scrapers'
 
+    def add_arguments(self, parser):
+        """take optional positional arguments"""
+        parser.add_argument("-s", "--scraper", dest='scraper', type=str, help="scraper's name")
+
     def handle(self, *args, **options):
         """run command"""
+        scrapers = SCRAPERS
+
+        if options['scraper']:
+            scrapers = [s for s in SCRAPERS if s.NAME == options['scraper']]
+
         # initialize scheduler
         sched = ScraperScheduler()
 
         # add scraping tasks
-        for scp in SCRAPERS:
+        for scp in scrapers:
+            tracker = ScrapeTracker.objects.filter(name=scp.NAME).first()
+            tracker.last_run = datetime.now()
+            tracker.status = 'running'
+            tracker.save()
+
             sched.add_task(scp().run())
 
         # run scheduler
         try:
             sched.run()
+
+            for scp in scrapers:
+                tracker = ScrapeTracker.objects.filter(name=scp.NAME).first()
+                tracker.status = 'finished'
+                tracker.save()
         except KeyboardInterrupt:
             print('quitting...\n')
             sys.exit(0)
