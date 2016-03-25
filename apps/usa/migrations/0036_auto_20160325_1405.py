86323f24a736879f7408f7f3d094a0662061de67
Matt Gathu <mattgathu@gmail.com>
2016-03-26 00:07:35 +0300

    usa app db migration

---
+++ apps/usa/migrations/0036_auto_20160325_1405.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('usa', '0035_auto_20160201_1846'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='fedoshaebsa',
+            options={'verbose_name_plural': 'OSHA Employee Benefits Security Adminstration Enforcements'},
+        ),
+    ]
