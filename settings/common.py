a5ebd04958c1099fed2f50b445c274902080cad5
Matt Gathu <mattgathu@gmail.com>
2016-04-04 05:12:28 +0300

    debug: renamed static folder to avoid error during collection

--- settings/common.py
+++ settings/common.py
@@ -104,7 +104,7 @@ MEDIA_URL = '/media/'
 
 ########## STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(DJANGO_ROOT, 'static'))
+STATIC_ROOT = normpath(join(DJANGO_ROOT, 'staticfiles'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
